381d4b9a27023628ea6d353b02cc093a4d861dc9
Alexander Bleissem <alexander@bleissem.com>
2016-09-22 23:26:26 +0200

    #1 - initial commit solution

---
+++ position.client/position.client.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using position.client.WinPhone.Resources;
+
+namespace position.client.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
